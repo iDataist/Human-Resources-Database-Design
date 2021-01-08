add77387f073ac3bf485a8fde6727dcc1d6dc2a8
iDataist <hwdgrmy@gmail.com>
2021-01-07 18:07:27 -0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,8 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "Employee" (
+﻿CREATE TABLE "Employee" (
     "Employee_ID" SERIAL   NOT NULL,
     "Employee_Name" VARCHAR(50)   NOT NULL,
     "Employee_Email" VARCHAR(50)   NOT NULL,
