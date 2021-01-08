cb64007a95e2a847fe82874ea41fa8b4b2ccfe5b
iDataist <hwdgrmy@gmail.com>
2021-01-08 10:20:24 -0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,100 +1,82 @@
-﻿CREATE TABLE "Employee" (
-    "Employee_ID" SERIAL   NOT NULL,
-    "Employee_Name" VARCHAR(50)   NOT NULL,
-    "Employee_Email" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Employee" PRIMARY KEY (
-        "Employee_ID"
-     )
-);
+﻿ALTER TABLE
+  employee
+ADD
+  CONSTRAINT pk_employee PRIMARY KEY (id);
 
-CREATE TABLE "Education" (
-    "Education_ID" SERIAL   NOT NULL,
-    "Education_Level" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Education" PRIMARY KEY (
-        "Education_ID"
-     )
-);
+ALTER TABLE
+  education
+ADD
+  CONSTRAINT pk_education PRIMARY KEY (education_id);
 
-CREATE TABLE "Job" (
-    "Job_ID" SERIAL   NOT NULL,
-    "Job_Title" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Job" PRIMARY KEY (
-        "Job_ID"
-     )
-);
+ALTER TABLE
+  job
+ADD
+  CONSTRAINT pk_job PRIMARY KEY (job_id);
 
-CREATE TABLE "Department" (
-    "Department_ID" SERIAL   NOT NULL,
-    "Department_Name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Department" PRIMARY KEY (
-        "Department_ID"
-     )
-);
+ALTER TABLE
+  department
+ADD
+  CONSTRAINT pk_department PRIMARY KEY (department_id);
 
-CREATE TABLE "Location" (
-    "Location_ID" SERIAL   NOT NULL,
-    "Location_Name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Location" PRIMARY KEY (
-        "Location_ID"
-     )
-);
+ALTER TABLE
+  location
+ADD
+  CONSTRAINT pk_location PRIMARY KEY (location_id);
 
-CREATE TABLE "Address" (
-    "Address_ID" SERIAL   NOT NULL,
-    "Address" VARCHAR(50)   NOT NULL,
-    "City" VARCHAR(50)   NOT NULL,
-    "State" CHAR(2)   NOT NULL,
-    "Location_ID" INT   NOT NULL,
-    CONSTRAINT "pk_Address" PRIMARY KEY (
-        "Address_ID"
-     )
-);
+ALTER TABLE
+  address
+ADD
+  CONSTRAINT pk_address PRIMARY KEY (address_id);
 
-CREATE TABLE "Salary" (
-    "Salary_ID" SERIAL   NOT NULL,
-    "Salary" NUMERIC   NOT NULL,
-    CONSTRAINT "pk_Salary" PRIMARY KEY (
-        "Salary_ID"
-     )
-);
+ALTER TABLE
+  salary
+ADD
+  CONSTRAINT pk_salary PRIMARY KEY (salary_id);
 
-CREATE TABLE "Employee_History" (
-    "Employee_ID" INT   NOT NULL,
-    "Manager_ID" INT   NOT NULL,
-    "Education_ID" INT   NOT NULL,
-    "Job_ID" INT   NOT NULL,
-    "Department_ID" INT   NOT NULL,
-    "Address_ID" INT   NOT NULL,
-    "Salary_ID" INT   NOT NULL,
-    "Hire_Date" DATE   NOT NULL,
-    "Start_Date" DATE   NOT NULL,
-    "End_Date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employee_History" PRIMARY KEY (
-        "Employee_ID","Job_ID","Department_ID"
-     )
-);
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT pk_employee_history PRIMARY KEY (
+    id, job_id, department_id
+  )
 
-ALTER TABLE "Address" ADD CONSTRAINT "fk_Address_Location_ID" FOREIGN KEY("Location_ID")
-REFERENCES "Location" ("Location_ID");
+ALTER TABLE
+  address
+ADD
+  CONSTRAINT fk_address_location_id FOREIGN KEY(location_id) REFERENCES location (location_id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Employee_ID" FOREIGN KEY("Employee_ID")
-REFERENCES "Employee" ("Employee_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_employee_id FOREIGN KEY(id) REFERENCES employee (id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Manager_ID" FOREIGN KEY("Manager_ID")
-REFERENCES "Employee" ("Employee_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_manager_id FOREIGN KEY(manager_id) REFERENCES employee (id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Education_ID" FOREIGN KEY("Education_ID")
-REFERENCES "Education" ("Education_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_education_id FOREIGN KEY(education_id) REFERENCES education (education_id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Job_ID" FOREIGN KEY("Job_ID")
-REFERENCES "Job" ("Job_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_job_id FOREIGN KEY(job_id) REFERENCES job (job_id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Department_ID" FOREIGN KEY("Department_ID")
-REFERENCES "Department" ("Department_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_department_id FOREIGN KEY(department_id) REFERENCES department (department_id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Address_ID" FOREIGN KEY("Address_ID")
-REFERENCES "Address" ("Address_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_address_id FOREIGN KEY(address_id) REFERENCES address (address_id);
 
-ALTER TABLE "Employee_History" ADD CONSTRAINT "fk_Employee_History_Salary_ID" FOREIGN KEY("Salary_ID")
-REFERENCES "Salary" ("Salary_ID");
+ALTER TABLE
+  employee_history
+ADD
+  CONSTRAINT fk_employee_history_salary_id FOREIGN KEY(salary_id) REFERENCES salary (salary_id);
 
